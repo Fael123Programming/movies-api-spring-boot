eeb02fb498407ac695ed6beec9d2240a5f520a54
Fael123Programming <rafaelfonseca1020@gmail.com>
2024-01-02 16:49:26 -0300

    Feat.: Movie domain

---
+++ src/main/java/dev/leafar/movies/Movie.java
@@ -0,0 +1,23 @@
+package dev.leafar.movies;
+
+import lombok.Data;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Document(collection = "movies")
+@Data
+public class Movie {
+    @Id
+    private ObjectId id;
+    private String imdbId;
+    private String title;
+    private String releaseDate;
+    private String trailerLink;
+    private String poster;
+    private List<String> genres;
+    private List<String> backdrops;
+    private List<String> reviewIds;
+}
