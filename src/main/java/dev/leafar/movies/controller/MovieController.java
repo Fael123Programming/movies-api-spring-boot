321319ca426c70abfe508583fa4b351362f595c5
Fael123Programming <rafaelfonseca1020@gmail.com>
2024-01-07 11:50:20 -0300

    Fixed api access

--- src/main/java/dev/leafar/movies/controller/MovieController.java
+++ src/main/java/dev/leafar/movies/controller/MovieController.java
@@ -5,10 +5,7 @@ import dev.leafar.movies.service.MovieService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,6 +13,7 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/api/v1/movies")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class MovieController {
     private final MovieService movieService;
     @GetMapping
--- src/main/java/dev/leafar/movies/controller/ReviewController.java
+++ src/main/java/dev/leafar/movies/controller/ReviewController.java
@@ -5,16 +5,14 @@ import dev.leafar.movies.service.ReviewService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/reviews")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class ReviewController {
     private final ReviewService reviewService;
 
